235b745ed66bb5dafbd0175e085b5a935aa182a9
darthevandar <andersundheim@gmail.com>
2015-04-27 10:06:05 -0500

    Removed testing code

--- src/fizzbuzz.java
+++
@@ -1,6 +0,0 @@
-public class fizzbuzz {
-	public static void main(String[] args){
-		for(int i=1;i<=100;i++)
-			System.out.println(i%15==0?"Fizz-Buzz":i%3==0?"Fizz":i%5==0?"Buzz":i);								
-	}
-}
